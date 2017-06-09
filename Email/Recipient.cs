c30145e65f9406f5bfba1a4cfe4e4df64ce409e6
Daren May <daren.may@gmail.com>
2017-06-09 15:52:47 -0600

    Cleanup

---
+++ Email/Recipient.cs
@@ -0,0 +1,19 @@
+﻿using Template10.Mvvm;
+
+namespace Template10.SampleData.Email
+{
+    public class Recipient : BindableBase
+    {
+        internal Recipient(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public string Email => $"{Name.Replace(" ", "_")}@email.com";
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Generator/Names/NameList.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Template10.SampleData.Generator.Names
+{
+    public class NameList : ObservableCollection<Name>
+    {
+        public NameList()
+        {
+            Add(new Name("Walter Olson"));
+            Add(new Name("Addyson Hudson"));
+            Add(new Name("Carson Riley"));
+            Add(new Name("Axel Mcmahon"));
+            Add(new Name("Holly Mccoy"));
+            Add(new Name("Amani Rosales"));
+            Add(new Name("Marlene Deleon"));
+            Add(new Name("Sierra Ali"));
+            Add(new Name("Hannah Christensen"));
+            Add(new Name("Cordell Andrews"));
+            Add(new Name("Jacqueline Grant"));
+            Add(new Name("Aidyn Tate"));
+            Add(new Name("Natalya Morton"));
+            Add(new Name("Jaylan Atkins"));
+            Add(new Name("Kali Charles"));
+            Add(new Name("Elisa Lucero"));
+            Add(new Name("Ryker Franco"));
+            Add(new Name("Aditya Bird"));
+            Add(new Name("Gage Chase"));
+            Add(new Name("Halle Phelps"));
+            Add(new Name("Gisselle Webster"));
+            Add(new Name("Nikolai Warner"));
+            Add(new Name("Aleena Mckinney"));
+            Add(new Name("Amanda Mcclure"));
+            Add(new Name("Nathan Cantu"));
+            Add(new Name("Sean Petersen"));
+            Add(new Name("Ramon Frank"));
+            Add(new Name("Rowan Hays"));
+            Add(new Name("Sarai Price"));
+            Add(new Name("Kaleigh Wiggins"));
+            Add(new Name("Israel Santos"));
+            Add(new Name("Mackenzie Brandt"));
+            Add(new Name("Emmanuel Campbell"));
+            Add(new Name("Mikayla Strong"));
+            Add(new Name("Natasha Ramirez"));
+            Add(new Name("Ben Mclean"));
+            Add(new Name("Aidan Stone"));
+            Add(new Name("Kiana Costa"));
+            Add(new Name("Nylah Stuart"));
+            Add(new Name("Abril Berg"));
+            Add(new Name("Jase Chavez"));
+            Add(new Name("Mark Lara"));
+            Add(new Name("Cesar Greer"));
+            Add(new Name("Aria Medina"));
+            Add(new Name("Lilah Williams"));
+            Add(new Name("Ivan Solomon"));
+            Add(new Name("Mateo Perry"));
+            Add(new Name("Winston Boyle"));
+        }
+    }
+}
